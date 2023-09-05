95def4b1ca626ecb9b54d28d30a72749a894d5e2
Alexandre <Alechemin@gmail.com>
2023-09-05 14:21:56 +0200

    Avancement ECF symfony

---
+++ src/DataFixtures/TestFixtures.php
@@ -0,0 +1,499 @@
+<?php
+
+namespace App\DataFixtures;
+
+use DateTime;
+use App\Entity\Genre;
+use App\Entity\Livre;
+use App\Entity\Auteur;
+use App\Entity\Emprunt;
+use App\Entity\Emprunteur;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as FakerFactory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+// $repotags = $this->manager->getRepository(Tag::class);
+// $ltags = $repotags->finAll();
+// $this->faker->randomElement($ltags);
+
+// $htmlTag = $repository->find(1);
+// $cssTag = $repository->find(2);
+// $jslTag = $repository->find(3);
+
+// éléments de code réutiliser dans vos boucles
+// $html = $tags[0];
+// $html->getName();
+// $tags[0]->getName();
+
+
+
+class TestFixtures extends Fixture implements FixtureGroupInterface
+{
+    private $faker;
+    private $hasher;
+    private $manager;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->faker = FakerFactory::create('fr_FR');
+        $this->hasher = $hasher;
+    }
+
+    public static function getGroups(): array
+    {
+        return ['test'];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        $this->manager = $manager;
+        $this->loadGenres();
+        $this->loadLivres();
+        // $this->loadAuteurs();
+        // $this->loadEmrprunts();
+        // $this->loadEmprunteurs();
+    }
+
+    public function loadLivres()
+    {
+        $repoEmprunt = $this->manager->getRepository(Emprunt::class);
+        $emprunt = $repoGenres->findAll();
+
+        $repoGenres = $this->manager->getRepository(Genre::class);
+        $genres = $repoGenres->findAll();
+        // $this->faker->randomElement($tags);
+
+        // on récupère un tag a partir de son id
+        $manga = $repoGenres->find(1);
+        $policier = $repoGenres->find(2);
+        $sf = $repoGenres->find(2);
+
+        $datas = [
+            [
+                'titre' => 'Moi, moche et méchant',
+                'anneeEdition' => 1990,
+                'nombrePage' => 100,
+                'codeIsbn' => null,
+                'genres' => [$manga],
+                // 'emprunt' => [],
+                'auteur' => null,
+            ],
+            [
+                'titre' => 'Arsène lupin',
+                'anneeEdition' => '2000',
+                'nombrePage' => 'Toto',
+                'codeIsbn' => null,
+                'genres' => [$policier],
+                // 'emprunt' => [],
+                'auteur' => null,
+            ],
+            [
+                'titre' => 'Starfield',
+                'anneeEdition' => null,
+                'nombrePage' => 'Toto',
+                'codeIsbn' => null,
+                'genres' => [$sf],
+                // 'emprunt' => [],
+                'auteur' => null,
+            ],
+        ];
+
+        // données statique 
+
+        foreach ($datas as $data) {
+            $livre = new Livre();
+            $livre->setName($data['name']);
+            $livre->setDescription($data['description']);
+            $livre->setClientName($data['clientName']);
+            $livre->setStartDate($data['startDate']);
+            $livre->setCheckpointDate($data['checkpointDate']);
+            $livre->setDeliveryDate($data['deliveryDate']);
+
+            foreach ($data['genres'] as $genre) {
+                $livre->addGenre($genre);
+            }
+
+            $this->manager->persist($livre);
+        }
+
+        $this->manager->flush();
+
+        // données dynamique
+
+        for ($i = 0; $i < 30; $i++) {
+            $livre = new Livre();
+            $words = random_int(1, 2);
+            $livre->setName($this->faker->sentence($words));
+            $words = random_int(2, 10);
+            $livre->setDescription($this->faker->optional(0.7)->sentence($words));
+            $livre->setClientName($this->faker->name());
+            $startDate = $this->faker->dateTimeBetween('-1 year', '-8months');
+            $livre->setStartDate($startDate);
+            $checkpointDate = $this->faker->dateTimeBetween('-7 months', '-2months');
+            $livre->setCheckpointDate($checkpointDate);
+            $deliveryDate = $this->faker->dateTimeBetween('-1 months', 'now');
+            $livre->setDeliveryDate($deliveryDate);
+
+            // on choisit le nombre de tags au hasard entre 1 et 4
+            $tagsCount = random_int(1, 4);
+            // on choisit des tags au hasard depuis la liste complète
+            $shortList = $this->faker->randomElements($tags, $tagsCount);
+
+            // on passe revue chaque tag de la short list
+            foreach ($shortList as $tag) {
+                // on associe un tag avec le projet
+                $livre->addTag($tag);
+            }
+
+            $this->manager->persist($livre);
+        }
+
+        $this->manager->flush();
+    }
+
+    // public function loadAuteurs()
+    // {
+    //     $repoTags = $this->manager->getRepository(Tag::class);
+    //     $tags = $repoTags->findAll();
+    //     // $this->faker->randomElement($tags);
+
+    //     // on récupère un tag a partir de son id
+    //     $htmlTag = $repoTags->find(1);
+    //     $cssTag = $repoTags->find(2);
+
+    //     // on récupère 
+    //     $jsTag = $tags[2];
+
+    //     $datas = [
+    //         [
+    //             'name' => 'P11',
+    //             'description' => null,
+    //             'clientName' => 'Toto',
+    //             'startDate' => new DateTime('2022-01-01'),
+    //             'checkpointDate' => new DateTime('2022-06-01'),
+    //             'deliveryDate' => new DateTime('2023-01-01'),
+    //             'tags' => [$htmlTag],
+    //         ],
+    //         [
+    //             'name' => 'P12',
+    //             'description' => null,
+    //             'clientName' => 'Titi',
+    //             'startDate' => new DateTime('2022-01-01'),
+    //             'checkpointDate' => new DateTime('2022-06-01'),
+    //             'deliveryDate' => new DateTime('2023-01-01'),
+    //             'tags' => [$cssTag],
+    //         ],
+    //         [
+    //             'name' => 'P13',
+    //             'description' => null,
+    //             'clientName' => 'Tata',
+    //             'startDate' => new DateTime('2022-01-01'),
+    //             'checkpointDate' => new DateTime('2022-06-01'),
+    //             'deliveryDate' => new DateTime('2023-01-01'),
+    //             'tags' => [$jsTag],
+    //         ],
+    //     ];
+
+    //     // données statique 
+
+    //     foreach ($datas as $data) {
+    //         $project = new Auteur();
+    //         $project->setName($data['name']);
+    //         $project->setDescription($data['description']);
+    //         $project->setClientName($data['clientName']);
+    //         $project->setStartDate($data['startDate']);
+    //         $project->setCheckpointDate($data['checkpointDate']);
+    //         $project->setDeliveryDate($data['deliveryDate']);
+
+    //         foreach ($data['tags'] as $tag) {
+    //             $project->addTag($tag);
+    //         }
+
+    //         $this->manager->persist($project);
+    //     }
+
+    //     $this->manager->flush();
+
+    //     // données dynamique
+
+    //     for ($i = 0; $i < 30; $i++) {
+    //         $project = new Auteur();
+    //         $words = random_int(1, 2);
+    //         $project->setName($this->faker->sentence($words));
+    //         $words = random_int(2, 10);
+    //         $project->setDescription($this->faker->optional(0.7)->sentence($words));
+    //         $project->setClientName($this->faker->name());
+    //         $startDate = $this->faker->dateTimeBetween('-1 year', '-8months');
+    //         $project->setStartDate($startDate);
+    //         $checkpointDate = $this->faker->dateTimeBetween('-7 months', '-2months');
+    //         $project->setCheckpointDate($checkpointDate);
+    //         $deliveryDate = $this->faker->dateTimeBetween('-1 months', 'now');
+    //         $project->setDeliveryDate($deliveryDate);
+
+    //         // on choisit le nombre de tags au hasard entre 1 et 4
+    //         $tagsCount = random_int(1, 4);
+    //         // on choisit des tags au hasard depuis la liste complète
+    //         $shortList = $this->faker->randomElements($tags, $tagsCount);
+
+    //         // on passe revue chaque tag de la short list
+    //         foreach ($shortList as $tag) {
+    //             // on associe un tag avec le projet
+    //             $project->addTag($tag);
+    //         }
+
+    //         $this->manager->persist($project);
+    //     }
+
+    //     $this->manager->flush();
+    // }
+
+
+
+    // public function loadEmrprunts()
+    // {
+    //     $datas = [
+    //         [
+    //             'name' => 'Alan turing',
+    //             'description' => null,
+    //             'startDate' => new DateTime('2022-01-01'),
+    //             'endDate' => new DateTime('2022-12-31'),
+    //         ],
+    //         [
+    //             'name' => 'John Von Neuman',
+    //             'description' => null,
+    //             'startDate' => new DateTime('2022-01-01'),
+    //             'endDate' => new DateTime('2022-12-31'),
+    //         ],
+    //         [
+    //             'name' => 'Brendan Eich',
+    //             'description' => null,
+    //             'startDate' => new DateTime('2022-01-01'),
+    //             'endDate' => new DateTime('2022-12-31'),
+    //         ],
+    //     ];
+
+    //     // données statique 
+
+    //     foreach ($datas as $data) {
+    //         $schoolyear = new Emprunt();
+    //         $schoolyear->setName($data['name']);
+    //         $schoolyear->setDescription($data['description']);
+    //         $schoolyear->setStartDate($data['startDate']);
+    //         $schoolyear->setEndDate($data['endDate']);
+
+    //         $this->manager->persist($schoolyear);
+    //     }
+
+    //     $this->manager->flush();
+
+    //     // données dynamique
+
+    //     for ($i = 0; $i < 10; $i++) {
+    //         $schoolyear = new Emprunt();
+    //         $words = random_int(2, 4);
+    //         $schoolyear->setName($this->faker->unique()->sentence($words));
+    //         $words = random_int(2, 10);
+    //         $schoolyear->setDescription($this->faker->optional(0.7)->sentence($words));
+
+    //         $startDate = $this->faker->dateTimeBetween('-1 year', '- 6months');
+    //         $schoolyear->setStartDate($startDate);
+
+    //         $endDate = $this->faker->dateTimeBetween('- 3 months', 'now');
+    //         $schoolyear->setEndDate($endDate);
+
+    //         $this->manager->persist($schoolyear);
+    //     }
+    //     $this->manager->flush();
+    // }
+
+    public function loadGenres()
+    {
+        // $repository = $this->manager->getRepository(Livre::class);
+        // $livres = $repository->findAll();
+        // // $this->faker->randomElement($livres);
+
+        // // on récupère un tag a partir de son id
+        // $livre1 = $repository->find(1);
+        // $livre2 = $repository->find(2);
+        // $livre3 = $repository->find(3);
+
+
+        // données statique
+        $datas = [
+            [
+                'nom' => 'Manga',
+                'description' => 'Livre d\'animation',
+                // 'livres' => [$livre1],
+            ],
+            [
+                'nom' => 'Policer',
+                'description' => 'Thriller policier',
+                // 'livres' => [$livre2],
+            ],
+            [
+                'nom' => 'Science fiction',
+                'description' => 'Livre imaginaire',
+                // 'livres' => [$livre3],
+            ],
+        ];
+        foreach ($datas as $data) {
+            $genre = new Genre();
+            $genre->setNom($data['nom']);
+            $genre->setDescription($data['description']);
+
+            // foreach ($data['livres'] as $livre) {
+            //     $genre->addLivre($livre);
+            // }
+
+            $this->manager->persist($genre);
+        }
+        $this->manager->flush();
+
+        //données dynamique
+        for ($i = 0; $i < 10; $i++) {
+            $genre = new Genre();
+            $words = random_int(1, 3);
+            $genre->setNom($this->faker->unique()->sentence($words));
+            $words = random_int(8, 15);
+            $genre->setDescription($this->faker->sentence($words));
+
+            
+            $livresCount = random_int(1, 2);
+            // on choisit des tags au hasard depuis la liste complète
+            // $shortList = $this->faker->randomElements($livres, $livresCount);
+
+            // on passe revue chaque tag de la short list
+            // foreach ($shortList as $livre) {
+            //     // on associe un tag avec le projet
+            //     $genre->addLivre($livre);
+            // }
+
+            $this->manager->persist($genre);
+        }
+        $this->manager->flush();
+    }
+
+    // public function loadEmprunteurs(): void
+    // {
+    //     $repoSchoolYear = $this->manager->getRepository(SchoolYear::class);
+    //     $schoolYears = $repoSchoolYear->findAll();
+    //     $repoTag = $this->manager->getRepository(Tag::class);
+    //     $tags = $repoTag->findAll();
+    //     $repoProject = $this->manager->getRepository(Project::class);
+    //     $projects = $repoProject->findAll();
+
+    //     $siteVitrine = $repoProject->find(1);
+    //     $wordPress = $repoProject->find(2);
+    //     $apiRest = $repoProject->find(3);
+
+    //     $htmlTag = $repoTag->find(1);
+    //     $cssTag = $repoTag->find(2);
+    //     $jsTag = $repoTag->find(3);
+
+    //     // Données statiques
+    //     $datas = [
+    //         [
+    //             'email' => 'alice@example.com',
+    //             'password' => '123',
+    //             'roles' => ['ROLE_USER'],
+    //             'firstName' => 'Foo',
+    //             'lastName' => 'Example',
+    //             'schoolYear' => $schoolYears[0],
+    //             'projects' => [$siteVitrine],
+    //             'tags' => [$htmlTag],
+    //         ],
+    //         [
+    //             'email' => 'bob@example.com',
+    //             'password' => '123',
+    //             'roles' => ['ROLE_USER'],
+    //             'firstName' => 'Bar',
+    //             'lastName' => 'Example',
+    //             'schoolYear' => $schoolYears[1],
+    //             'projects' => [$wordPress],
+    //             'tags' => [$cssTag, $htmlTag],
+    //         ],
+    //         [
+    //             'email' => 'charlie@example.com',
+    //             'password' => '123',
+    //             'roles' => ['ROLE_USER'],
+    //             'firstName' => 'Baz',
+    //             'lastName' => 'Example',
+    //             'schoolYear' => $schoolYears[2],
+    //             'projects' => [$apiRest],
+    //             'tags' => [$jsTag],
+    //         ],
+    //     ];
+
+    //     foreach ($datas as $data) {
+
+    //         $user = new User();
+    //         $user->setEmail($data['email']);
+    //         $password = $this->hasher->hashPassword($user, $data['password']);
+    //         $user->setPassword($password);
+    //         $user->setRoles($data['roles']);
+    //         // Persist sert a stocker l'user dans la base de donner
+    //         $this->manager->persist($user);
+
+    //         $student = new Emprunteur();
+    //         $student->setFirstName($data['firstName']);
+    //         $student->setLastName($data['lastName']);
+    //         $student->setSchoolYear($data['schoolYear']);
+    //         $student->setUser($user);
+
+    //         // récupération du premier projet de la liste du student
+    //         $project = $data['projects'][0];
+    //         $student->addProject($project);
+
+
+    //         foreach ($data['tags'] as $tag) {
+    //             $student->addTag($tag);
+    //         }
+    //         // $tag = $data['tags'][0];
+    //         // $student->addTag($tag);
+
+
+    //         $this->manager->persist($student);
+    //     }
+
+
+    //     $this->manager->flush();
+
+    //     // flush = push du user dans la base de donner
+
+    //     // // données dynamiques
+    //     for ($i = 0; $i < 50; $i++) {
+    //         $user = new User();
+    //         $user->setEmail($this->faker->unique()->safeEmail());
+    //         $password = $this->hasher->hashPassword($user, $data['password']);
+    //         $user->setPassword($password);
+    //         $user->setRoles(['ROLE_USER']);
+
+    //         $student = new Student();
+    //         $student->setFirstname($this->faker->firstName());
+    //         $student->setLastname($this->faker->lastName());
+
+    //         $schoolYear = $this->faker->randomElement($schoolYears);
+    //         $student->setSchoolYear($schoolYear);
+
+    //         $project = $this->faker->randomElement($projects);
+    //         $student->addProject($project);
+
+    //         $tagsCount = random_int(1, 4);
+    //         $shortList = $this->faker->randomElements($tags, $tagsCount);
+    //         foreach ($shortList as $tag) {
+    //             $student->addTag($tag);
+    //         }
+
+    //         $student->setUser($user);
+
+
+
+    //         $this->manager->persist($user);
+    //     }
+    //     $this->manager->flush();
+    // }
+}
